be350a9719fc58bff18d751cfddfe997ff988531
Katerina Ilieva <61324205+Kativa7@users.noreply.github.com>
2021-06-08 14:45:44 +0300

    Add files via upload

---
+++ config/express.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+
+module.exports = (app) => {
+    app.engine(
+        "hbs",
+        hbs({
+          extname: ".hbs",
+        })
+      );
+      app.set("view engine", "hbs"); //auto .hbs
+      app.use("/static", express.static("static"));
+      app.use(express.urlencoded({ extended: false }));
+}
