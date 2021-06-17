1599a1c5449d19904db3ee5e87257a2ab06dd983
Katerina Ilieva <61324205+Kativa7@users.noreply.github.com>
2021-06-17 21:08:45 +0300

    Add files via upload

--- config/express.js
+++ config/express.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const hbs = require("express-handlebars");
+const auth = require("../middlewares/auth");
+const cookieParser = require("cookie-parser");
 
 module.exports = (app) => {
-    app.engine(
-        "hbs",
-        hbs({
-          extname: ".hbs",
-        })
-      );
-      app.set("view engine", "hbs"); //auto .hbs
-      app.use("/static", express.static("static"));
-      app.use(express.urlencoded({ extended: false }));
-}
+  app.engine(
+    "hbs",
+    hbs({
+      extname: ".hbs",
+    })
+  );
+  app.set("view engine", "hbs"); //auto .hbs
+  app.use("/static", express.static("static"));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+
+  app.use(auth());
+};
---
+++ controllers/authController.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+
+router.get("/register", (req, res) => {
+  res.render("register", { title: "Register Page" });
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    await req.auth.register(req.body);
+    res.redirect("/products");
+  } catch (err) {
+    const ctx = {
+      title: "Register",
+      error: err.message,
+      data: { username: req.body.username },
+    };
+    res.render("register", ctx);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login Page" });
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    await req.auth.login(req.body);
+    res.redirect("/products");
+  } catch (err) {
+    const ctx = {
+      title: "Login",
+      error: err.message,
+      data: { username: req.body.username },
+    };
+    res.render("login", ctx);
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.auth.logout();
+  res.redirect("/products");
+});
+
+module.exports = router;
